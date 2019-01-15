8ad08f660ed5379bb298cbdf5a8e6e3b533c85e1
Benjamin <benjamin.chastanier@gmail.com>
2019-01-15 14:41:20 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# Add vim options
+wget https://raw.githubusercontent.com/benjaminch/vim-setup/master/my_configs.vim -O ~/.vim_runtime/my_configs.vim
+
+# Add plugins
 while read repo; do
-    git clone "$repo" my_plugings/
-done < my_plugings.txt
+    git clone "$repo" ~/.vim_runtime/my_plugins/
+done < my_plugins.txt
